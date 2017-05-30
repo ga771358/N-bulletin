a7dc0d87dc79b98f705dfb6008688a14c4c0d1d2
劉沂寬 <ga771358@gmail.com>
2017-05-30 13:46:47 +0800

    Create OK

--- app/controllers/bulletin_controller.rb
+++ app/controllers/bulletin_controller.rb
@@ -5,5 +5,15 @@ class BulletinController < ApplicationController
     def new
         @boards = Board.new
     end
+    def create
+        @boards = Board.new(board_params)
+        @boards.save
+        redirect_to bulletin_index_path
+    end
+
+    private
+    def board_params
+    params.require(:board).permit(:description,:year,:month,:day)
+    end
     
 end
